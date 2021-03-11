88f39eeeeac2020942828e60a4dd95eebf052717
Ezequiel Merino <emerino@aistechnology.es>
2021-03-11 15:34:17 +0100

    add barcodes

--- bcr.py
+++ bcr.py
@@ -1,4 +1,13 @@
 while True:
     codereaded = input('Enter your input:')
-    print ('%s \n' % (codereaded))
+    if codereaded == "qL0aX0cP1mC9vQ9w":
+        print ('w\n')
+    elif codereaded == "nV2pZ0gJ8hN9zX0j":
+        print ('a\n')
+    elif codereaded == "bZ6rN6xU4fC4wH2s":
+        print ('s\n')
+    elif codereaded == "cR4zK4tX2dZ4sQ9j":
+        print ('d\n')
+    elif codereaded == "aA9iI6yB7bR2pO4j":
+        print ('_\n')
 
