7df8e174cee704782980880524a84c2c0d86c731
Ezequiel Merino <emerino@aistechnology.es>
2021-03-12 14:26:36 +0100

    change to json as default to control the barcodes

--- bcr.py
+++ bcr.py
@@ -1,10 +1,13 @@
 import win32gui
 import win32con
 import win32api
+import json
 from time import sleep
 
 arraywindowsopen = []
 windowtosend = ""
+defaultbarcodes = '{ "w":"qL0aX0cP1mC9vQ9w", "a":"nV2pZ0gJ8hN9zX0j", "s":"cR4zK4tX2dZ4sQ9j", "d":"cR4zK4tX2dZ4sQ9j", "_":"aA9iI6yB7bR2pO4j"}'
+usedbarcodes = json.loads(defaultbarcodes)
 
 def winEnumHandler( hwnd, ctx ):
     if win32gui.IsWindowVisible( hwnd ):
@@ -25,19 +28,19 @@ hwndChild = win32gui.GetWindow(hwndMain, win32con.GW_CHILD)
 
 while True:
     codereaded = input('Enter your input:')
-    if codereaded == "qL0aX0cP1mC9vQ9w":
+    if codereaded == usedbarcodes["w"]:
         temp = win32api.PostMessage(hwndChild, win32con.WM_CHAR, 0x77, 0)
         print ('w\n')
-    elif codereaded == "nV2pZ0gJ8hN9zX0j":
+    elif codereaded == usedbarcodes["a"]:
         temp = win32api.PostMessage(hwndChild, win32con.WM_CHAR, 0x61, 0)
         print ('a\n')
-    elif codereaded == "bZ6rN6xU4fC4wH2s":
+    elif codereaded == usedbarcodes["s"]:
         temp = win32api.PostMessage(hwndChild, win32con.WM_CHAR, 0x73, 0)
         print ('s\n')
-    elif codereaded == "cR4zK4tX2dZ4sQ9j":
+    elif codereaded == usedbarcodes["d"]:
         temp = win32api.PostMessage(hwndChild, win32con.WM_CHAR, 0x64, 0)
         print ('d\n')
-    elif codereaded == "aA9iI6yB7bR2pO4j":
+    elif codereaded == usedbarcodes["_"]:
         temp = win32api.PostMessage(hwndChild, win32con.WM_CHAR, 0x20, 0)
         print ('_\n')
 
