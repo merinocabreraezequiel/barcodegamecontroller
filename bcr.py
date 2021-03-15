f136ab5720adfc16ec9ec20599214c9b97609b6d
Ezequiel Merino <emerino@aistechnology.es>
2021-03-15 10:50:31 +0100

    resolve application not Notepad Open

--- bcr.py
+++ bcr.py
@@ -6,6 +6,7 @@ from time import sleep
 
 arraywindowsopen = []
 windowtosend = ""
+hwndChild = ""
 defaultbarcodes = '{ "w":"qL0aX0cP1mC9vQ9w", "a":"nV2pZ0gJ8hN9zX0j", "s":"cR4zK4tX2dZ4sQ9j", "d":"cR4zK4tX2dZ4sQ9j", "_":"aA9iI6yB7bR2pO4j"}'
 usedbarcodes = json.loads(defaultbarcodes)
 
@@ -16,31 +17,38 @@ def winEnumHandler( hwnd, ctx ):
             arraywindowsopen.append(windowsopen)
             print (len(arraywindowsopen), arraywindowsopen[len(arraywindowsopen)-1])
 
-win32gui.EnumWindows( winEnumHandler, None )
+def run():
+    global hwndChild
+    win32gui.EnumWindows( winEnumHandler, None )
 
-windowselected = input("Select Window ID: ")
-if int(windowselected) < len(arraywindowsopen):
-    print ('Selected Window: ', arraywindowsopen[int(windowselected)-1],'\n')
-    windowtosend = arraywindowsopen[int(windowselected)-1]
+    windowselected = input("Select Window ID: ")
+    if int(windowselected) < len(arraywindowsopen):
+        print ('Selected Window: ', arraywindowsopen[int(windowselected)-1],'\n')
+        windowtosend = arraywindowsopen[int(windowselected)-1]
 
-hwndMain = win32gui.FindWindow("Notepad", windowtosend)
-hwndChild = win32gui.GetWindow(hwndMain, win32con.GW_CHILD)
+    hwndMain = win32gui.FindWindow(None, windowtosend)
+    hwndChild = win32gui.GetWindow(hwndMain, win32con.GW_CHILD)
 
-while True:
-    codereaded = input('Enter your input:')
-    if codereaded == usedbarcodes["w"]:
-        temp = win32api.PostMessage(hwndChild, win32con.WM_CHAR, 0x77, 0)
-        print ('w\n')
-    elif codereaded == usedbarcodes["a"]:
-        temp = win32api.PostMessage(hwndChild, win32con.WM_CHAR, 0x61, 0)
-        print ('a\n')
-    elif codereaded == usedbarcodes["s"]:
-        temp = win32api.PostMessage(hwndChild, win32con.WM_CHAR, 0x73, 0)
-        print ('s\n')
-    elif codereaded == usedbarcodes["d"]:
-        temp = win32api.PostMessage(hwndChild, win32con.WM_CHAR, 0x64, 0)
-        print ('d\n')
-    elif codereaded == usedbarcodes["_"]:
-        temp = win32api.PostMessage(hwndChild, win32con.WM_CHAR, 0x20, 0)
-        print ('_\n')
+    sendControl()
 
+def sendControl():
+    while True:
+        codereaded = input('Enter your input:')
+        if codereaded == usedbarcodes["w"]:
+            temp = win32api.PostMessage(hwndChild, win32con.WM_CHAR, 0x77, 0)
+            print ('w\n')
+        elif codereaded == usedbarcodes["a"]:
+            temp = win32api.PostMessage(hwndChild, win32con.WM_CHAR, 0x61, 0)
+            print ('a\n')
+        elif codereaded == usedbarcodes["s"]:
+            temp = win32api.PostMessage(hwndChild, win32con.WM_CHAR, 0x73, 0)
+            print ('s\n')
+        elif codereaded == usedbarcodes["d"]:
+            temp = win32api.PostMessage(hwndChild, win32con.WM_CHAR, 0x64, 0)
+            print ('d\n')
+        elif codereaded == usedbarcodes["_"]:
+            temp = win32api.PostMessage(hwndChild, win32con.WM_CHAR, 0x20, 0)
+            print ('_\n')
+
+if __name__ == '__main__':
+    run()
